b72385ee9296dc6ce671692843a5f6a8feb55c88
Sümeyye Ünver <93975563+smyynvr@users.noreply.github.com>
2022-06-22 10:46:50 +0300

    Update jdk.pp

--- bigtop-deploy/puppet/manifests/jdk.pp
+++ bigtop-deploy/puppet/manifests/jdk.pp
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$jdk_preinstalled = hiera("bigtop::jdk_preinstalled", true)
+$jdk_preinstalled = hiera("bigtop::jdk_preinstalled", false)
 
 class jdk {
   case $::operatingsystem {
